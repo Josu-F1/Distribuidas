90371896030c9f18a4427ae0566067cbb1dcace4
Josue Fiallos <164249945+Josu-F1@users.noreply.github.com>
2026-04-24 00:35:41 +0000

    revisa el stock

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def listar_productos():
         cursor = conn.cursor()
 
         cursor.execute("""
-            SELECT TOP 20 id, nombre, precio
+            SELECT TOP 20 id, nombre, precio, stock
             FROM productos
             ORDER BY id DESC
         """)
@@ -102,7 +102,7 @@ def listar_productos():
                 "id": row[0],
                 "nombre": row[1],
                 "precio": float(row[2]) if row[2] is not None else None,
-            
+                "stock": row[3]
             })
 
         return jsonify({
